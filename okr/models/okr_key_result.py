6fc118069e72465e4b97507b7ec14ab2271d4aa2
Glenda Viera Delgado <gvd@adhoc.inc>
2026-04-22 14:56:27 -0300

    [IMP] okr: added constraint to target and weight of KRs

--- okr/models/okr_key_result.py
+++ okr/models/okr_key_result.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from odoo.exceptions import ValidationError
+
+from odoo import api, fields, models
 
 
 class OKRKeyResult(models.Model):
@@ -26,3 +28,24 @@ class OKRKeyResult(models.Model):
         string="State",
         default="draft",
     )
+
+    _check_target = models.Constraint(
+        "CHECK(target >= 0 and target <= 100)",
+        "Target must be between 0 and 100.",
+    )
+
+    @api.model
+    def create(self, vals_list):
+        key_results = self.env["okr.key_result"].search(
+            [("objective_id", "=", vals_list[0].get("objective_id"))]
+        )
+        total_weight = sum(kr.weight for kr in key_results)
+        for vals in vals_list:
+            if vals.get("target") < 0 or vals.get("target") > 100:
+                raise ValidationError("Key result target must be between 0 and 100.")
+            total_weight += vals.get("weight")
+            if total_weight > 100:
+                raise ValidationError(
+                    "Total weight of key results for an objective cannot exceed 100%."
+                )
+        return super().create(vals_list)
