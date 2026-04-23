e12e6ea61a3740036172d3d486dc97402c542d63
Glenda Viera Delgado <gvd@adhoc.inc>
2026-04-23 09:28:09 -0300

    [IMP] okr: Calculation of the period according to the selected cadence

--- okr/models/okr.py
+++ okr/models/okr.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from odoo.tools import date_utils
+
+from odoo import api, fields, models
 
 
 class Okr(models.Model):
@@ -29,7 +31,37 @@ class Okr(models.Model):
         default="yearly",
     )
     in_charge_id = fields.Many2one("res.users", string="In Charge")
-    start_date = fields.Date(string="Start Date", readonly=True)
-    end_date = fields.Date(string="End Date", readonly=True)
+    start_date = fields.Date(string="Start Date", compute="_compute_period")
+    end_date = fields.Date(string="End Date", readonly=True, compute="_compute_period")
     parent_id = fields.Many2one("okr", string="Parent OKR")
     child_ids = fields.One2many("okr", "parent_id", string="Child OKRs")
+
+    @api.depends("cadence")
+    def _compute_period(self):
+        today = fields.Date.today()
+        current_month = today.month
+
+        # Trimestre actual
+        current_q = (current_month - 1) // 3
+        cadence_map = {"q1": 0, "q2": 1, "q3": 2, "q4": 3}
+
+        for okr in self:
+            if okr.cadence == "yearly":
+                next_year = today.replace(year=today.year + 1)
+                okr.start_date = date_utils.start_of(next_year, "year")
+                okr.end_date = date_utils.end_of(next_year, "year")
+
+            else:
+                quarter = cadence_map.get(okr.cadence)
+
+                # Diferencia entre trimestres
+                diff = quarter - current_q
+
+                # Si ya pasó o se está transitando, se pasa al siguiente año
+                if diff <= 0:
+                    diff += 4
+
+                # Cálculo de fechas
+                target = date_utils.add(today, months=diff * 3)
+                okr.start_date = date_utils.start_of(target, "quarter")
+                okr.end_date = date_utils.end_of(target, "quarter")
--- okr/models/okr_objective.py
+++ okr/models/okr_objective.py
@@ -1,3 +1,5 @@
+from odoo.tools import date_utils
+
 from odoo import api, fields, models
 
 
@@ -52,3 +54,33 @@ class OKRObjective(models.Model):
                 )
             else:
                 objective.result = 0.0
+
+    @api.depends("cadence")
+    def _compute_period(self):
+        today = fields.Date.today()
+        current_month = today.month
+
+        # Trimestre actual
+        current_q = (current_month - 1) // 3
+        cadence_map = {"q1": 0, "q2": 1, "q3": 2, "q4": 3}
+
+        for okr in self:
+            if okr.cadence == "yearly":
+                next_year = today.replace(year=today.year + 1)
+                okr.start_date = date_utils.start_of(next_year, "year")
+                okr.end_date = date_utils.end_of(next_year, "year")
+
+            else:
+                quarter = cadence_map.get(okr.cadence)
+
+                # Diferencia entre trimestres
+                diff = quarter - current_q
+
+                # Si ya pasó o se está transitando, se pasa al siguiente año
+                if diff <= 0:
+                    diff += 4
+
+                # Cálculo de fechas
+                target = date_utils.add(today, months=diff * 3)
+                okr.start_date = date_utils.start_of(target, "quarter")
+                okr.end_date = date_utils.end_of(target, "quarter")
